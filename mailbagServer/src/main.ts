1e6b2666fe004c692ba9594da8d3087cf4c5b0b3
Adam Brittain <adsy.britt@gmail.com>
2020-07-23 16:42:45 +1000

    CORS handling
    Added code to handle the CORS limitations for dev

---
+++ mailbagServer/src/main.ts
@@ -0,0 +1,34 @@
+import path from "path";
+import express, {
+  Express,
+  NextFunction,
+  Request,
+  Response,
+  response,
+} from "express";
+import { ServerInfo } from "./ServerInfo";
+import * as IMAP from "./IMAP";
+import * as SMTP from "./SMTP";
+import * as Contacts from "./contacts";
+import { IContact } from "./contacts";
+import { request } from "http";
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.use("/", express.static(path.join(__dirname, "../../client/dist")));
+
+app.use(function (
+  inRequest: Request,
+  inResponse: Response,
+  inNext: NextFunction
+) {
+  inResponse.header("Access-Control-Allow-Origin", "*");
+  inResponse.header("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS");
+  inResponse.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  inNext();
+});
